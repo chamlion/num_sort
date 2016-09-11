227f145cddfdee25d60a53024fa800834eadb524
chamlion <chamlionchina@gmail.com>
2016-09-11 22:07:36 +0800

    functions change ,test change

--- thread_poll_test.cc
+++ thread_poll_test.cc
@@ -6,7 +6,7 @@
 #include "num_sort.h"
 #include "functions.h"
 /*
-测试说明： split -l 1000000 out.txt -d -a 2 data_  将原数据分割
+测试说明： split -l 10000000 out.txt -d -a 2 data_  将原数据分割
 保存在data文件夹下
 
 */
@@ -15,7 +15,7 @@ int main()
   std::shared_ptr<num::time_past> newtime(new num::time_past);
   std::vector<std::string> datas;
   std::string str;
-  num:: get_all_file_name(datas);
+  num:: get_all_file_name(datas,"data1");
   std::shared_ptr<num::thread_poll> pool(new num::thread_poll(4));
  std::vector<num::num_sort*> sorts;
   pool->start();
@@ -25,8 +25,9 @@ for(auto iter=datas.begin();iter != datas.end();iter++)
  // std::string path_read("data/"+*iter);
  // std::string path_write("data/result"+(*iter));
 //std::cout<<"before push_back"<<std::endl;
-//std::cout<<*iter<<std::endl;
-  sorts.push_back( new num::num_sort(("data/"+*iter),("data/result_"+(*iter))));
+std::cout<<*iter<<std::endl;
+ 
+  sorts.push_back( new num::num_sort(("data1/"+*iter),("result_data/result_"+(*iter))));
 //std::cout<<"after push_back"<<std::endl;
 }
 //std::cout<<"sorts create ok"<<std::endl;
@@ -36,5 +37,6 @@ for(auto iter=sorts.begin();iter != sorts.end();iter++)
 }
   pool->stop();
 }
+
 
 
